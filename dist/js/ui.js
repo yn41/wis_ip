7773b9097abaea45c0a371c5b571cbeb72867010
yuna <yuna@wisdomain.com>
2023-11-24 10:04:42 +0900

    모듈 완료 특허페이지 진행중

--- dist/js/ui.js
+++ dist/js/ui.js
@@ -54,31 +54,33 @@ $(function(){
 		scrollPosition = 0;
 	});
 	
-	//셀랙트 박스 세팅
-	// for (var i =0; i < $(".slct").length; i++) {
-	// 	var target = $(".slct").eq(i);
-	// 	if($.trim(target.find(".slct_tit").text()) === "")
-	// 		target.find(".slct_tit").text(target.find(".slct_cont .select").text());
-		
-	// }
-	// $(".slct_tit").on("click", function (e) {
-	// 	if($(this).parent().hasClass("on") === true) {
-	// 		$(this).parent().removeClass("on");
-	// 	}else {
-	// 		$(".slct").removeClass("on");
-	// 		$(this).parent().addClass("on");
-	// 	}
-	// });
-	// $(".slct_cont li").on("click", function(){
-	// 	$(this).parents(".slct").find(".slct_tit").text($(this).text());
-	// 	$(this).parent().find("li").removeClass("select");
-	// 	$(this).addClass("select");
-	// 	$(this).parents(".slct").removeClass("on");
-	// });
-	// $(document).on("click", function(e){
-	// 	if(e.target.className === "slct" || e.target.className === "slct_tit" || e.target.className === "slct_cont") {return false;}
-	// 	$(".slct").removeClass("on");
-	// });
+	// S | v:pat1 - 셀랙트 박스 세팅
+	for (var i =0; i < $(".bx_slct").length; i++) {
+		var target = $(".bx_slct").eq(i);
+		if($.trim(target.find(".slct_tit").text()) === "")
+		target.find(".slct_tit").text(target.find(".slct_cont .select").text());
+}
+	$(".slct_tit").on("click", function (e) {
+		if($(this).parent().hasClass("dis") !== true){
+			if($(this).parent().hasClass("on") === true) {
+				$(this).parent().removeClass("on");
+			}else {
+				$(".bx_slct").removeClass("on");
+				$(this).parent().addClass("on");
+			}
+		}
+	});
+	$(".slct_cont li").on("click", function(){
+		$(this).parents(".bx_slct").find(".slct_tit").text($(this).text());
+		$(this).parent().find("li").removeClass("select");
+		$(this).addClass("select");
+		$(this).parents(".bx_slct").removeClass("on");
+	});
+	$(document).on("click", function(e){
+		if(e.target.className === "bx_slct" || e.target.className === "slct_tit" || e.target.className === "slct_cont") {return false;}
+		$(".bx_slct").removeClass("on");
+	});
+	// E | v:pat1 - 셀랙트 박스 세팅
 });
 // $(window).resize(function(){
 // });
