6ae36246f14e315283bc4b345d6f4d1e58d28a72
yuna <yuna@wisdomain.com>
2023-11-30 10:01:22 +0900

    아이콘 정리

--- dist/js/ui.js
+++ dist/js/ui.js
@@ -55,7 +55,6 @@ $(function(){
 		scrollPosition = 0;
 	});
 	
-	// S | v:pat1 - 셀랙트 박스 세팅
 	for (var i =0; i < $(".bx_slct").length; i++) {
 		var target = $(".bx_slct").eq(i);
 		if($.trim(target.find(".slct_tit").text()) === "")
@@ -81,7 +80,13 @@ $(function(){
 		if(e.target.className === "bx_slct" || e.target.className === "slct_tit" || e.target.className === "slct_cont") {return false;}
 		$(".bx_slct").removeClass("on");
 	});
-	// E | v:pat1 - 셀랙트 박스 세팅
 });
-// $(window).resize(function(){
-// });
+// S | v:pat2 - gnb 반응형 대응
+$(window).resize(function(){
+	if($( window ).width() > 1003 && $(".header").hasClass("open")){
+		$(".header").removeClass("open");
+		$("body").css({"overflow":"", "top":""});
+		scrollPosition = 0;
+	}
+});
+// E | v:pat2 -  gnb 반응형 대응
